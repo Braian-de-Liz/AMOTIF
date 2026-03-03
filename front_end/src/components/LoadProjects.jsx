3e127b00fbacf508a2b97cac5a4c403476d53c52
Braian-de-Liz <delizbraian@gmail.com>
2026-03-02 21:04:02 -0300

    hub de urls

---
+++ front_end/src/components/LoadProjects.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { URL_API_TESTE, UPLOAD_URL_teste } from "../utility/url_apis";
+
+
+function ProjetosLoader() {
+
+    async function get_projects(e) {
+
+    }
+
+    return (
+        <>
+            
+        </>
+    )
+
+}
+
+export { ProjetosLoader };
--- front_end/src/utility/url_apis.js
+++ front_end/src/utility/url_apis.js
@@ -1,5 +1,8 @@
+// Hub de Urls das APIs
 const URL_API = "https://amotif.onrender.com/api";
 const URL_API_TESTE = "http://localhost:3333/api";
+const UPLOAD_URL_teste = "http://localhost:4444";
+const UPLOAD_URL = "";
 
 
-export { URL_API, URL_API_TESTE };
+export { URL_API, URL_API_TESTE, UPLOAD_URL_teste, UPLOAD_URL };
