e0739f41e1d7ed269fb58763dbc631daf378f436
Braian-de-Liz <delizbraian@gmail.com>
2026-06-11 20:49:57 -0300

    health simplificada

--- back_end/src/routers/health/health.ts
+++ back_end/src/routers/health/health.ts
@@ -1,25 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
-import { Type } from '@sinclair/typebox';
 
 const health_route: FastifyPluginAsyncTypebox = async (Fastify) => {
-    Fastify.get("/health", {
-        schema: {
-            tag: ['testes'],
-            response: {
-                200: Type.Object({
-                    status: Type.String(),
-                    timestamp: Type.String({ format: "date-time" }),
-                    uptime: Type.Number()
-                })
-            }
-        }
-    }, async (request, reply) => {
-        return reply.status(200).send({
-            status: "ok",
-            timestamp: new Date().toISOString(),
-            uptime: process.uptime()
-        });
+
+    Fastify.get("/health",  async (request, reply) => {
+
+        return reply.status(200).send("ping");
     });
+
 };
 
 export { health_route };
